c4fe2311d45ee8c37f7558fbee57cbe9cfb594dc
mmeganshaw <mmeganshaw@gmail.com>
2018-03-09 23:53:31 -0500

    committing almost all files

--- giphy.js
+++ giphy.js
@@ -6,7 +6,8 @@
 
       var searchfor = $(this).attr("data-name");
       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchfor + "&api_key=civx0SbZOudPUd2wINsO1BTGTpqYrJHe&limit=10";
-
+      empty();
+      
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -40,47 +41,57 @@
 
   }
 
-// Function for displaying movie data
+
       function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
         $("#searches").empty();
 
-        // Looping through the array of movies
         for (var i = 0; i < searches.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
           var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("gif-btn");
-          // Adding a data-attribute
+
+          a.addClass("btn");
+
           a.attr("data-name", searches[i]);
-          // Providing the initial button text
+
           a.text(searches[i]);
-          // Adding the button to the buttons-view div
+
           $("#searches").append(a);
         }
       }
 
-      // This function handles events where a movie button is clicked
+
+      function empty () {
+
+        $('#gifone').empty();
+        $('#giftwo').empty();
+        $('#gifthree').empty();
+        $('#giffour').empty();
+        $('#gifive').empty();
+        $('#gifsix').empty();
+        $('#gifseven').empty();
+        $('#gifeight').empty();
+        $('#gifnine').empty();
+        $('#giften').empty();
+
+      }
+
+
+
       $("#submit").on("click", function(event) {
+
+
         event.preventDefault();
-        // This line grabs the input from the textbox
+
         var searchfor = $("#input").val().trim();
 
-        // Adding movie from the textbox to our array
         searches.push(searchfor);
 
-        // Calling renderButtons which handles the processing of our movie array
         renderButtons();
       });
   
 
-      // Adding a click event listener to all elements with a class of "movie-btn"
-      $(document).on("click", ".gif-btn", displayGifInfo);
+      $(document).on("click", ".btn", displayGifInfo);
 
-      // Calling the renderButtons function to display the intial buttons
   
       renderButtons();
